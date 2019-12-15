d43232f111595bc706fe2e9e2e69304c387584d4
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-12-15 14:50:42 +0100

    Added GPLv3 headers
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- include/cdf.hpp
+++ include/cdf.hpp
@@ -1,24 +1,40 @@
-#include <string>
-#include <optional>
-#include <unordered_map>
+/*------------------------------------------------------------------------------
+-- This file is a part of the CDFpp library
+-- Copyright (C) 2019, Plasma Physics Laboratory - CNRS
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+-------------------------------------------------------------------------------*/
+/*-- Author : Alexis Jeandet
+-- Mail : alexis.jeandet@member.fsf.org
+----------------------------------------------------------------------------*/
 #include "attribute.hpp"
-#include "variable.hpp"
 #include "cdf-io.hpp"
+#include "variable.hpp"
+#include <optional>
+#include <string>
+#include <unordered_map>
 
 namespace cdf {
-    struct CDF
-    {
-        std::unordered_map<std::string, Variable> variables;
-        std::unordered_map<std::string, Attribute> attrinutes;
-        const Variable& operator[](const std::string& name)const
-        {
-            return variables.at(name);
-        }
-    };
+struct CDF {
+  std::unordered_map<std::string, Variable> variables;
+  std::unordered_map<std::string, Attribute> attrinutes;
+  const Variable &operator[](const std::string &name) const {
+    return variables.at(name);
+  }
+};
 
-    std::optional<CDF> open(const std::string& path)
-    {
-        return std::nullopt;
-    }
+std::optional<CDF> open(const std::string &path) { return std::nullopt; }
 
-}
+} // namespace cdf
