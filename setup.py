5dbdff3a5986d69b21f3fdaa57d6763beba17fc4
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-01-17 15:42:03 +0100

    Switched back to cmake for building PyPi wheels until it works with Meson
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+
+setup(
+    name="pycdfpp",
+    version="0.0.1",
+    description="A modern C++ header only cdf library",
+    author='Alexis Jeandet',
+    license="GNU General Public License v3",
+    packages=['pycdfpp']
+)
