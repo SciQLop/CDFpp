770627e856c2010eec3fdf77d427be67cb1f0a98
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-01-19 16:58:39 +0100

    Improved setup.py and ready to test automatic push on PyPi
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from skbuild import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="pycdfpp",
-    version="0.0.1",
+    version="0.1.0",
     description="A modern C++ header only cdf library",
     author='Alexis Jeandet',
+    author_email='alexis.jeandet@member.fsf.org',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+        ],
     license="GNU General Public License v3",
+    long_description=readme,
+    keywords='CDF NASA Space Physics Plasma',
+    url='https://github.com/SciQLop/CDFpp',
     packages=['pycdfpp']
 )
