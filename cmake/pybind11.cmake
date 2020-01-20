cd5d1228af485484aa4c5e505ce7f3928294d2b8
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-01-20 11:10:33 +0100

    Simpler build for python packages
    - disabled werror with scikit-build
    - disabled tests build with scikit-build
    - automatic Pybind11 clone if not available
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

---
+++ cmake/pybind11.cmake
@@ -0,0 +1,9 @@
+find_package(pybind11 QUIET)
+if (NOT pybind11_FOUND)
+    execute_process(
+        COMMAND git clone --branch v2.4.3 https://github.com/pybind/pybind11 ${CMAKE_CURRENT_BINARY_DIR}/pybind11
+        )
+    add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/pybind11 ${CMAKE_CURRENT_BINARY_DIR}/build_pybind11)
+endif()
+
+
