56eddcf1c1dd030813c9e75a44613f79e17aa27b
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-03-05 22:38:50 +0100

    Fixed stupid mistake inside unit tests
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/simple_open/main.cpp
+++ tests/simple_open/main.cpp
@@ -120,14 +120,17 @@ bool check_time_variable(const cdf::Variable& var, std::initializer_list<uint32_
 {
     using namespace date;
     using namespace std::chrono;
+    auto offset=0;
+    if constexpr (std::is_same_v<time_t,cdf::tt2000_t >)
+        offset=5;//skip 1971 issue with tt2000
     auto values = std::vector<decltype(cdf::to_time_point(time_t {}))>();
     std::transform(std::cbegin(var.get<time_t>()), std::cend(var.get<time_t>()),
         std::back_inserter(values), [](const time_t& v) { return cdf::to_time_point(v); });
     bool is_valid = compare_shape(var, expected_shape);
     auto ref = std::vector<decltype(cdf::to_time_point(time_t {}))>(std::size(values));
     std::generate(std::begin(ref), std::end(ref),
-        [i = 0]() mutable { return cdf::constants::_1970 + days(i * 180) + 0ns; });
-    is_valid &= std::inner_product(std::begin(ref), std::end(ref), std::begin(values), is_valid,
+        [i = 0]() mutable { return cdf::constants::_1970 + days(i++ * 180) + 0ns;});
+    is_valid &= std::inner_product(std::begin(ref)+offset, std::end(ref), std::begin(values)+offset, is_valid,
         std::logical_and<>(), std::equal_to<>());
     return is_valid;
 }
