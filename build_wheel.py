86e8fa45f08f29288cd0473a4cfca16a3fbfae6d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-08-15 12:57:53 +0200

    Just rename wheel using distutils to cheat and get the correct name
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_wheel.py
+++ build_wheel.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
-from mesonpy import build_wheel, build_sdist
 import os
+from glob import glob
+import sys
+import subprocess
+from distutils.core import Extension
+from setuptools.dist import Distribution
 
-if not os.path.exists('dist'):
-    os.mkdir('dist')
+def wheel_name(name, version):
+    # taken from https://stackoverflow.com/questions/60643710/setuptools-know-in-advance-the-wheel-filename-of-a-native-library
+    fuzzlib = Extension('fuzzlib', ['fuzz.pyx'])  # the files don't need to exist
+    dist = Distribution(attrs={'name': name, 'version': version, 'ext_modules': [fuzzlib]})
+    bdist_wheel_cmd = dist.get_command_obj('bdist_wheel')
+    bdist_wheel_cmd.ensure_finalized()
 
-build_wheel('dist')
+    distname = bdist_wheel_cmd.wheel_dist_name
+    tag = '-'.join(bdist_wheel_cmd.get_tag())
+    return f'{distname}-{tag}.whl'
+
+def build_wheel():
+    subprocess.run([sys.executable, "-m", "build", "."])
+
+def fix_wheel_name(wheel):
+    name, version = wheel.split('-')[:2]
+    new_name = wheel_name(name, version)
+    print(f"New wheel name: dist{os.path.sep}{new_name}")
+    os.rename(f'dist{os.path.sep}{wheel}', f'dist{os.path.sep}{new_name}')
+
+if __name__ == '__main__':
+
+    wheels_before = glob('dist/*.whl')
+    build_wheel()
+    wheels_after = glob('dist/*.whl')
+    diff = list(set(wheels_after) - set(wheels_before))
+
+    if len(diff) == 1:
+        print(f"Found one wheel to rename {diff[0]}")
+        fix_wheel_name(os.path.basename(diff[0]))
